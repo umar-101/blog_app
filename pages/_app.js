10708ee6f1fbeebb7bfc1356570f936ab0c48eb8
UMAR <umarmushtaq101@gmail.com>
2022-06-11 11:49:10 +0500

    navbar added

---
+++ pages/_app.js
@@ -0,0 +1,14 @@
+import "../styles/globals.scss";
+
+import React, { useState, useEffect } from "react";
+
+import { Layout } from "../components";
+function MyApp({ Component, pageProps }) {
+  return (
+    <Layout>
+      <Component {...pageProps} />
+    </Layout>
+  );
+}
+
+export default MyApp;
