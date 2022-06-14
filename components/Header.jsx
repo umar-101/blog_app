35cc3c56aef9a3456140d70863945414e6e4b450
UMAR <umarmushtaq101@gmail.com>
2022-06-14 11:00:23 +0500

    categories added

--- components/Header.jsx
+++ components/Header.jsx
@@ -1,14 +1,17 @@
-import React from 'react'
 
-import { useContext } from 'react'
+import React, { useEffect, useState } from 'react'
 import Link from 'next/link'
+import { getCategories } from '../services'
 
-const Categories = [
-    { name: 'React', slug: 'react' },
-    { name: 'Node', slug: 'node' },
-]
 
 const Header = () => {
+
+
+    const [categories, setCategories] = useState([]);
+    useEffect(() => {
+        getCategories()
+            .then((newCategories) => setCategories(newCategories))
+    }, [])
     return (
         <div className='container mx-auto px-10 mb-8'>
             <div className='border-b w-full inline-block border-blue-400 py-8'>
@@ -22,7 +25,7 @@ const Header = () => {
                 </div>
 
                 <div className='hidden md:float-left md:contents'>
-                    {Categories.map((category) => (
+                    {categories.map((category) => (
                         <Link key={category.slug} href={`category/${category.slug}`}>
                             <span className='md:float-right mt-2 align-middle text-white ml-4 font-semibold cursor-pointer'>
                                 {category.name}
